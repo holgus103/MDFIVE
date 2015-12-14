d457e03a68684e2f0370f4ab7bdbda8084511b9e
Holger <holgus103@gmail.com>
2015-12-14 21:46:17 +0100

    Done - more or less - ASM and C++ implementation - ASM is faster than C++

--- MDFIVE/Source.cpp
+++ MDFIVE/Source.cpp
@@ -1,6 +1,4 @@
 ﻿
-
-#define ASM
 #include <windows.h>
 #include <winbase.h>
 #include <time.h>
@@ -8,50 +6,6 @@
 
 
 int main(){
-	int F = 0, B = 0xefcdab89, C = 0x98badcfe, D = 0x10325476;
-	clock_t time, end;
-	typedef int(*MD5Func)(int B, int C, int D);
-	HMODULE hModule = LoadLibrary(TEXT("MDFIVEDll.dll"));
-	MD5Func FFunc = (MD5Func)GetProcAddress(hModule, "FFunc");
-	MD5Func GFunc = (MD5Func)GetProcAddress(hModule, "GFunc");
-	MD5Func HFunc = (MD5Func)GetProcAddress(hModule, "HFunc");
-	MD5Func IFunc = (MD5Func)GetProcAddress(hModule, "IFunc");
-	time = clock();
-	for (int i = 0; i < 10000000;i++)
-	F = FFunc(B, C,D);
-	end = clock();
-	std::cout << "F asm: " <<  end - time << std::endl;
-	time = clock();
-	for (int i = 0; i < 10000000; i++)
-	F = (B & C) | ((~B) & D);
-	end = clock();
-	std::cout << "F c: " << end - time << std::endl;
-	/*time = clock();
-	F = GFunc(B, C, D);
-	end = clock();
-	std::cout << "G asm: " << time - end << std::endl;
-	time = clock();
-	F = (D & B) | ((~D) & C);
-	end = clock();
-	std::cout << "G c: " << time - end << std::endl;
-	time = clock();
-	F = HFunc(B, C, D);
-	end = clock();
-	std::cout << "H asm: " << time - end << std::endl;
-	time = clock();
-	F = B ^ C ^ D;
-	end = clock();
-	std::cout << "H c: " << time - end << std::endl;
-	time = clock();
-	F = IFunc(B, C, D);
-	end = clock();
-	std::cout << "I asm: " << time - end << std::endl;
-	time = clock();
-	F = C ^ (B | (~D));
-	end = clock();
-	std::cout << "I c: " << time - end << std::endl;
-	time = clock();*/
-	FreeModule(hModule);
-	//calculateHash("C:\\test.txt");
+	MD5::calculateHash("D:\\ISOS\\java.exe");
 	return 0;
 }
